c9802aca05f308b7f6ac0190407b0daa1bc1ce7e
eveille <leo.eveille@univ-rouen.fr>
2023-09-08 15:58:19 +0200

    5ème commit

--- TP1/fonctions.py
+++ TP1/fonctions.py
@@ -1,5 +1,28 @@
+#def puissance(a,b):
+#   if a < 0 and b == 0:
+#      return -1
+#   else:
+#      return a ** b
+      
 def puissance(a, b):
+    if not type(var) is int:
+        raise TypeError("Only integers are allowed")
+
+    if b == 0:
+        if a < 0:
+            res = -1
+        else 
+            res = 1
+     
+    if b < 0:
+        if a == 0:
+            return 0
+        a = 1 / a  
+        b = -b  
     
-    resultat = a ** b
+    resultat = 1
     
-    return resultat
+    for _ in range(b):
+        res *= a
+
+    return res
